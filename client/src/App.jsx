d6a560884dc832df3f87f7f1ae7cf9643780fe5b
ARYANPATILSOLOCODER11 <aryanpformal@gmail.com>
2024-03-19 20:11:44 +0530

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,10 @@
-<<<<<<< HEAD
-=======
-import { useState } from 'react'
->>>>>>> cb5f9e7516153c7576e6e1ae904d7972f0b0c17e
-import './App.css'
-import Register from './Pages/Register'
-import Login from './Pages/Login'
-function App() {
+import React from 'react'
 
+const App = () => {
   return (
-    <>
-<<<<<<< HEAD
-    <h1 className="text-3xl font-bold underline">
-    Hello world Aryan!
-  </h1>
-  <Register/>
-  <Login/>
-=======
-      <div>hello</div>
-      <div>hello Aryan </div>
-      <div>hello Tejas </div>
-
->>>>>>> cb5f9e7516153c7576e6e1ae904d7972f0b0c17e
-    </>
+    <div>
+      
+    </div>
   )
 }
 
