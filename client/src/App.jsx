a3a1ae04c7e1fcce7fdca3a01078d7d3a4c9bfba
ayush011-1 <136148962+ayush011-1@users.noreply.github.com>
2024-03-19 19:54:36 +0530

    see

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,18 @@
-<<<<<<< HEAD
-=======
 import { useState } from 'react'
->>>>>>> cb5f9e7516153c7576e6e1ae904d7972f0b0c17e
 import './App.css'
-import Register from './Pages/Register'
-import Login from './Pages/Login'
+
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
-    <h1 className="text-3xl font-bold underline">
-    Hello world Aryan!
-  </h1>
-  <Register/>
-  <Login/>
-=======
       <div>hello</div>
       <div>hello Aryan </div>
-      <div>hello Tejas </div>
-
->>>>>>> cb5f9e7516153c7576e6e1ae904d7972f0b0c17e
+      <div>hello ayush</div>
+      
+    
     </>
   )
 }
 
-export default App
+
