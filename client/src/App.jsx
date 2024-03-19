4f1d0c189b12a04e1cdbbad1bd73d92e54286b8c
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2024-03-19 20:15:34 +0530

    a

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,26 @@
-<<<<<<< HEAD
-=======
 import { useState } from 'react'
->>>>>>> cb5f9e7516153c7576e6e1ae904d7972f0b0c17e
 import './App.css'
 import Register from './Pages/Register'
 import Login from './Pages/Login'
 function App() {
   const [count, setCount] = useState(0)
->>>>>>> a3a1ae04c7e1fcce7fdca3a01078d7d3a4c9bfba
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
     <h1 className="text-3xl font-bold underline">
     Hello world Aryan!
   </h1>
   <Register/>
   <Login/>
-=======
       <div>hello</div>
       <div>hello Aryan </div>
       <div>hello Tejas </div>
 
->>>>>>> cb5f9e7516153c7576e6e1ae904d7972f0b0c17e
     </>
   )
 }
 
+}
 
+export default App;
