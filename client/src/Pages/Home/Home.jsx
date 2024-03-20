fdcf6df701f3b15e9d2263c2f578fb231ffb635c
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2024-03-20 14:38:35 +0530

    hero done

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,24 +1,38 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Home.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Home.css";
+import hero from "../../assets/hero.png";
+import logo from "../../assets/meetinglogo.png";
+
 const Home = () => {
   return (
     <div>
       <nav className="navbar navbar-dark ">
-          <div className="logo">logo</div>
-          <div className="loginbtns">
-            <button>Sign up</button>
-            <button>Sign in</button>
-          </div>
+        <div className="logo">
+          <img src={logo} />
+        </div>
+        <div className="loginbtns">
+          <button>Sign up</button>
+          <button>Sign in</button>
+        </div>
       </nav>
-      
+
       <div className="main-page">
         <div className="left">
-          <p>LESS MEETINGS<br/>MORE WORK DONE</p>
-          <button>New Meeting</button>
+          <p>
+            "Unlocking Productivity, One Meeting at a Time: Your AI Meeting
+            Companion"
+          </p>
+          <div className="btn-container">
+            <button>New Meeting</button>
+            <button>Join Meeting</button>
+          </div>
         </div>
         <div className="right">
-          <image src="" />
+          <img src={hero} className="hero-image rounded-md select-none" 
+          draggable="false"
+          alt="Hero Image"
+          />
         </div>
       </div>
     </div>
